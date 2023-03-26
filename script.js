eb3d12bfbca539f35f2b955050af3d63e8aa783e
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-26 11:50:10 +0200

    Removed for loop creating buttons

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 // Wrapped my Javascript to only execute after HTML is fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
-  //Create the number pad for calculator(not operator buttons)
-  let buttonContainer = document.getElementById("button-grid");
-  for (let i = 1; i <= 19; i++) {
-    let button = document.createElement("button");
-    button.textContent = i;
-    buttonContainer.appendChild(button);
-  }
-
   //Operate Function to be called when resultCall is clicked.
   function operate(num1, num2, operator) {
     switch (operator) {
