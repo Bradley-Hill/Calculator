8258465cd88a0dcc0c85750407f95931175a17de
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-26 18:33:24 +0200

    added function for

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Wrapped my Javascript to only execute after HTML is fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
-  //Create references to display and result button
+  //Create references to display-screen, result button and number buttons.
   let displayScreen = document.getElementById("display-screen");
   let resultCall = document.getElementById("result");
+  let numberButtons = document.querySelectorAll(".number");
 
   //Init variables for computation
   let numberOne = null;
   let numberTwo = null;
   let operatorChoice = null;
+
+  numberButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      displayScreen.textContent += button.textContent;
+    });
+  });
 
   //Operate Function to be called when result is clicked.
   function operate(num1, num2, operator) {
