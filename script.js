986e3dd86cb21796624de67deaf73e15883861b9
Bradley-Hill <97234446+Bradley-Hill@users.noreply.github.com>
2023-03-27 11:44:46 +0200

    added clearAll function

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 // Wrapped my Javascript to only execute after HTML is fully loaded.
 document.addEventListener("DOMContentLoaded", function () {
   //Create references to display-screen, result button and number buttons.
-  let displayScreen = document.getElementById("display-screen");
-  let resultCall = document.getElementById("result");
-  let numberButtons = document.querySelectorAll(".number");
+  const displayScreen = document.getElementById("display-screen");
+  const resultCall = document.getElementById("result");
+  const numberButtons = document.querySelectorAll(".number");
+  const clearButton = document.getElementById("clearAll");
 
   //Init variables for computation
   let numberOne = null;
   let numberTwo = null;
   let operatorChoice = null;
+  let decimalOne = false;
+  let decimalTwo = false;
 
   numberButtons.forEach(function (button) {
     button.addEventListener("click", function () {
-      displayScreen.textContent += button.textContent;
+      if (numberOne === null) {
+        numberOne = button.textContent;
+        displayScreen.textContent += button.textContent;
+      } else {
+        numberOne += button.textContent;
+        displayScreen.textContent += button.textContent;
+      }
     });
+  });
+
+  //Clear button event listener
+  clearButton.addEventListener("click", function () {
+    numberOne = null;
+    numberTwo = null;
+    operatorChoice = null;
+    decimalOne = false;
+    decimalTwo = false;
+    displayScreen.textContent = "";
   });
 
   //Operate Function to be called when result is clicked.
